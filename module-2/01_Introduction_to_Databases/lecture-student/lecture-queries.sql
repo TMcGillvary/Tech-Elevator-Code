4ae4d85c2c3e59ec39690b73e5afa885288ca84d
Tiffany McGillvary <tmcgillvary94@gmail.com>
2021-10-11 15:25:27 -0400

    all lecture and homework for M2D1

--- module-2/01_Introduction_to_Databases/lecture-student/lecture-queries.sql
+++ module-2/01_Introduction_to_Databases/lecture-student/lecture-queries.sql
@@ -1,41 +1,56 @@
 -- SELECT
 -- Use a SELECT statement to return a literal string
-
+SELECT 'Hello, world!';
+-- select 'Hello, world!'; -- is also possible
+-- Select 'Hello, world!'; -- is also possible
 
 -- Use a SELECT statement to add two numbers together (and label the result "sum")
-
+SELECT 3 + 5 AS sum;
 
 
 -- SELECT ... FROM
 -- Write queries to retrieve...
 
 -- The names from all the records in the state table
-
+SELECT state_name FROM state;
 
 -- The names and populations of all cities
-
+SELECT 
+  city_name, population
+FROM
+  city;
 
 -- All columns from the park table
-
+SELECT * from park; -- shorthand for all columns is * wildcard
 
 
 -- SELECT __ FROM __ WHERE
 -- Write queries to retrieve...
 
 -- The names of cities in California (CA)
+SELECT city_name FROM city WHERE state_abbreviation = 'CA';
+-- the data within the table is case sensitive, always single quotes string
 
 
 -- The names and state abbreviations of cities NOT in California
+SELECT city_name, state_abbreviation FROM city WHERE state_abbreviation <> 'CA';
+SELECT city_name, state_abbreviation FROM city WHERE state_abbreviation != 'CA';
+-- <> and != is the same thing
 
 
 -- The names and areas of cities smaller than 25 square kilometers 
+SELECT city_name, area FROM city WHERE area < 25.0;
+-- even though number is a numeric postgres can recognize 25 vs 25.0 as the same
 
 
 -- The names from all records in the state table that have no assigned census region
+SELECT state_name FROM state WHERE census_region IS NULL; -- Nothing equals NULL, including NULL. In other words NULL doesn't equal NULL
+-- have to do keyword 'IS' not '=' for null values
 
 
 -- The names and census regions from all records in the state table that have an assigned census region
-
+SELECT state_name, census_region FROM state WHERE census_region IS NOT NULL;
+-- the not equals or != equivalent is IS NOT
 
 
 -- WHERE with AND/OR
@@ -43,23 +58,26 @@
 
 -- The names, areas, and populations of cities smaller than 25 sq. km. with more than 100,000 people
 
+SELECT city_name, state_abbreviation, area, population FROM city 
+  WHERE area < 25.0 AND population > 100000;
 
 -- The names and census regions of all states (and territories and districts) not in the Midwest region (including those that don't have any census region)
-
+SELECT DISTINCT census_region FROM state;
+SELECT state_name, census_region FROM state WHERE census_region != 'Midwest' OR census_region IS NULL;
 
 -- The names, areas, and populations of cities in California (CA) or Florida (FL)
 
 
 -- The names, areas, and populations of cities in New England -- Connecticut (CT), Maine (ME), Massachusetts (MA), New Hampshire (NH), Rhode Island (RI) and Vermont (VT)
-
+SELECT city_name, area, population FROM city WHERE state_abbreviation IN ('CT', 'ME', 'MA', 'NH', 'RI', 'VT');
 
 
 -- SELECT statements involving math
 -- Write a query to retrieve the names and areas of all parks in square METERS
 -- (the values in the database are stored in square kilometers)
 -- Label the second column "area_in_meters"
-
-
+SELECT park_name, area FROM park;
+SELECT park_name, area * 1000 AS area_in_meters FROM park;
 
 -- All values vs. distinct values
 
@@ -74,20 +92,20 @@
 -- Write queries to retrieve...
 
 -- The names of all cities that begin with the letter "A"
-
+SELECT city_name FROM city WHERE city_name LIKE 'A%';
 
 -- The names of all cities that end with "Falls"
-
+SELECT city_name FROM city WHERE city_name LIKE '%Falls';
 
 -- The names of all cities that contain a space
-
+SELECT city_name FROM city WHERE city_name LIKE '% %';
 
 
 -- BETWEEN
 -- Write a query to retrieve the names and areas of parks of at least 100 sq. kilometers but no more than 200 sq. kilometers
-
+SELECT park_name, area FROM park WHERE area BETWEEN 100 AND 200;
 
 
 -- DATES
 -- Write a query to retrieve the names and dates established of parks established before 1900
-
+SELECT park_name, date_established FROM park WHERE date_established < '1900-01-01'; -- date literal syntax
