c3a7affbad9d589188d42c899da6a7f6228b3f41
Tiffany McGillvary <tmcgillvary94@gmail.com>
2021-11-21 22:41:14 -0500

    I am so supremely stuck on step 5 of the homework I'm at a complete deadlock... I'm going to need to ask on this tomorrow

--- module-3/11_Introduction_to_Vue_and_Data_Binding/exercise-student/src/components/UserList.vue
+++ module-3/11_Introduction_to_Vue_and_Data_Binding/exercise-student/src/components/UserList.vue
@@ -1,22 +1,30 @@
 <template>
   <table id="tblUsers">
     <thead>
-    <tr>
+      <tr>
         <th>First Name</th>
         <th>Last Name</th>
         <th>Username</th>
         <th>Email Address</th>
         <th>Status</th>
-    </tr>
+      </tr>
     </thead>
     <tbody>
       <tr>
-        <td><input type="text" id="firstNameFilter"/></td>
-        <td><input type="text" id="lastNameFilter"/></td>
-        <td><input type="text" id="usernameFilter"/></td>
-        <td><input type="text" id="emailFilter"/></td>
         <td>
-          <select id="statusFilter">
+          <input type="text" id="firstNameFilter" v-model="filter.firstName" />
+        </td>
+        <td>
+          <input type="text" id="lastNameFilter" v-model="filter.lastName" />
+        </td>
+        <td>
+          <input type="text" id="usernameFilter" v-model="filter.username" />
+        </td>
+        <td>
+          <input type="text" id="emailFilter" v-model="filter.emailAddress" />
+        </td>
+        <td>
+          <select id="statusFilter" v-model="filter.status">
             <option value="">Show All</option>
             <option value="Active">Active</option>
             <option value="Disabled">Disabled</option>
@@ -24,35 +32,95 @@
         </td>
       </tr>
       <!-- user listing goes here -->
+      <tr v-for="user in users" v-bind:key="user.username">
+        <td>{{ user.firstName }}</td>
+        <td>{{ user.lastName }}</td>
+        <td>{{ user.username }}</td>
+        <td>{{ user.emailAddress }}</td>
+        <td>{{ user.status }}</td>
+      </tr>
     </tbody>
   </table>
 </template>
 
 <script>
 export default {
-  name: 'user-list',
+  name: "user-list",
   data() {
     return {
+      filter: {
+        firstName: "",
+        lastName: "",
+        uername: "",
+        emailAddress: "",
+        status: "",
+      },
+
       users: [
-        { firstName: 'John', lastName: 'Smith', username: 'jsmith', emailAddress: 'jsmith@gmail.com', status: 'Active' },
-        { firstName: 'Anna', lastName: 'Bell', username: 'abell', emailAddress: 'abell@yahoo.com', status: 'Active' },
-        { firstName: 'George', lastName: 'Best', username: 'gbest', emailAddress: 'gbest@gmail.com', status: 'Disabled' },
-        { firstName: 'Ben', lastName: 'Carter', username: 'bcarter', emailAddress: 'bcarter@gmail.com', status: 'Active' },
-        { firstName: 'Katie', lastName: 'Jackson', username: 'kjackson', emailAddress: 'kjackson@yahoo.com', status: 'Active' },
-        { firstName: 'Mark', lastName: 'Smith', username: 'msmith', emailAddress: 'msmith@foo.com', status: 'Disabled' }
-      ]
-    }
-  }
-}
+        {
+          firstName: "John",
+          lastName: "Smith",
+          username: "jsmith",
+          emailAddress: "jsmith@gmail.com",
+          status: "Active",
+        },
+        {
+          firstName: "Anna",
+          lastName: "Bell",
+          username: "abell",
+          emailAddress: "abell@yahoo.com",
+          status: "Active",
+        },
+        {
+          firstName: "George",
+          lastName: "Best",
+          username: "gbest",
+          emailAddress: "gbest@gmail.com",
+          status: "Disabled",
+        },
+        {
+          firstName: "Ben",
+          lastName: "Carter",
+          username: "bcarter",
+          emailAddress: "bcarter@gmail.com",
+          status: "Active",
+        },
+        {
+          firstName: "Katie",
+          lastName: "Jackson",
+          username: "kjackson",
+          emailAddress: "kjackson@yahoo.com",
+          status: "Active",
+        },
+        {
+          firstName: "Mark",
+          lastName: "Smith",
+          username: "msmith",
+          emailAddress: "msmith@foo.com",
+          status: "Disabled",
+        },
+      ],
+    };
+  },
+
+  computed: {
+    filteredList() {
+      // i am just supremely stuck and have no idea how to do this in a way that will let you compare each key value pair to each other
+      // I can get it figured out easily to look at a single filter (i.e. firstName) and I can even extend it out to 2 filters, but I can't figure out how to do them all
+      // using a filter on the users makes sense, but I have no idea where to go from there as every single example I google is only comparing one filter value, not all of them
+    },
+  },
+};
 </script>
 
 <style scoped>
 table {
   margin-top: 20px;
-  font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 }
 th {
-  text-transform: uppercase
+  text-transform: uppercase;
 }
 td {
   padding: 10px;
@@ -60,7 +128,8 @@ td {
 tr.disabled {
   color: red;
 }
-input, select {
+input,
+select {
   font-size: 16px;
 }
 </style>
---
+++ module-3/11_Introduction_to_Vue_and_Data_Binding/lecture-student/script.js
@@ -0,0 +1,18 @@
+const myApp = new Vue({
+
+    data() {
+        return {
+            message: "Yay! It's Friday!",
+            firstName: "Dave",
+            planets: [
+                "Mercury",
+                "Venus",
+                "Earth"
+            ],
+            showDescription: false,
+
+        }
+    }
+});
+
+myApp.$mount("#myApp");
