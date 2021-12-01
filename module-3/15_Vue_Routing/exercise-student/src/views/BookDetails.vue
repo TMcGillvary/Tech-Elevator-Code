43318a825ab3d4292b5575cfe0531e2cb0eca4db
Tiffany McGillvary <tmcgillvary94@gmail.com>
2021-12-01 00:21:45 -0500

    all homework for vue routing, done!

---
+++ module-3/15_Vue_Routing/exercise-student/src/views/BookDetails.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="book-details">
+    <book-details />
+  </div>
+</template>
+
+<script>
+import BookDetails from "../components/BookDetails.vue";
+export default {
+  components: { BookDetails },
+  created() {
+    const bookISBN = this.$route.params.isbn;
+    this.$store.commit("SET_ACTIVE_BOOK", bookISBN);
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ module-3/15_Vue_Routing/exercise-student/src/views/NewBook.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="new-book">
+    <new-book-form />
+  </div>
+</template>
+
+<script>
+import NewBookForm from "@/components/NewBookForm.vue";
+export default {
+  components: { NewBookForm },
+};
+</script>
+
+<style>
+</style>
