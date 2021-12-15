67b98580ea1bfd6fac42e1e96939d1e0315168e4
Tiffany McGillvary <tmcgillvary94@gmail.com>
2021-12-15 17:52:43 -0500

    all the stuff i forgot to push during the last lecture day

--- capstone-student/java/database/dropdb.sql
+++ capstone-student/java/database/dropdb.sql
@@ -5,9 +5,9 @@
 -- The following line terminates any active connections to the database so that it can be destroyed
 SELECT pg_terminate_backend(pid)
 FROM pg_stat_activity
-WHERE datname = 'final_capstone';
+WHERE datname = 'capstone_student';
 
-DROP DATABASE final_capstone;
+DROP DATABASE capstone_student;
 
-DROP USER final_capstone_owner;
-DROP USER final_capstone_appuser;
+DROP USER capstone_student_owner;
+DROP USER capstone_student_appuser;
